97b2d17d313f94aca4c5969174b63fe1ec970b99
harishraju55 <harishraju55@gmail.com>
2017-05-31 12:00:57 -0500

    initial

--- src/main/java/com/tek/StocksMicroserviceApplication.java
+++ src/main/java/com/tek/StocksMicroserviceApplication.java
@@ -3,7 +3,6 @@ package com.tek;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.tek.model.Stock;
